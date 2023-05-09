2c29c255019f0a6aa962b3efadb5312484029c7d
theshariqueanwer <mdshariqueanwer01@gmail.com>
2023-05-10 01:55:16 +0530

    take feature commit

--- src/main/java/com/pack/user/api/controller/UserController.java
+++ src/main/java/com/pack/user/api/controller/UserController.java
@@ -4,10 +4,9 @@ package com.pack.user.api.controller;
 import com.pack.user.api.entity.User;
 import com.pack.user.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -19,6 +18,11 @@ public class UserController {
     @PostMapping("/postUser")
     public User postUser(@RequestBody User user) {
         return userService.postUser(user);
+    }
+
+    @GetMapping("/getAllUser")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
     }
 
 }
--- src/main/java/com/pack/user/api/service/UserService.java
+++ src/main/java/com/pack/user/api/service/UserService.java
@@ -3,7 +3,11 @@ package com.pack.user.api.service;
 
 import com.pack.user.api.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     public User postUser(User user);
+
+    public List<User> getAllUser();
 
 }
